0a27e53cf7d815e9027dffb66c542468db94667e
Tuvshuu Tsetsengarid <ttsets2@uic.edu>
2023-04-08 10:20:36 -0500

    Added commented code mongoDB fixed 5050 to 3000

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 
+// var MongoClient = require('mongodb').MongoClient;
+// var url = 'mongodb://localhost/chatDB'
+// MongoClient.connect(url, function(err, db){
+//     console.log("YES! SOMEHOW IT CONNECTED!");
+//     db.close();
+// });
+
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
 
-app.listen(5500, () => {
+app.listen(3000, () => {
     console.log('App listening on port 3000');
 });
